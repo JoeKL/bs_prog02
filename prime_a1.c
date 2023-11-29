c63b6ac6d989bac7de89d9586d66a529520d046b
Niko Tepe <niko.tepe@gmail.com>
2023-11-29 10:41:22 +0100

    a1 implemented

--- prime_a1.c
+++ prime_a1.c
@@ -2,13 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
-#define NUM_THREADS 5
-#define ARRAY_SIZE 4096
-
-int *PrimeArray = NULL; // Global pointer initialized to NULL
-int array_index = 0;
-
+unsigned int latest_prime = 0;
 
 bool isPrime(unsigned int number)
 {
@@ -25,19 +21,35 @@ bool isPrime(unsigned int number)
     return true;
 }
 
-
-void *serve_number(){
-    printf("starting server thread\n");
-
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        printf("checking %d\n", i);
-        if(isPrime(i)){
-            PrimeArray[array_index] = i;
-            array_index++;
-            printf("\t%d is prime!\n", i);
+//client läuft in dauerschleife und prüft ob latest_prime gesetzt ist, wenn ja dann print und lösche latest_prime
+void *print_prime(){
+    while(true){
+        if(latest_prime != 0){
+            printf("CLIENT: %d is prime!\n", latest_prime);
+            latest_prime = 0;
+            //der client soll für eine Sekunde warten, wenn prime geprintet wurde
+            sleep(1);
         }
+    }
+    return NULL;
+}
 
+// 
+void *find_primes(){
+
+    // zähle von 1 bis unendlich in dauerschleife
+    for (unsigned int i = 1; i; i++)
+    {
+        // überprüfe ob i Prime ist
+        if(isPrime(i)){
+            //wenn ja, speichere i als latest_prime, sodass sie vom Clienten geprintet werden kann
+            printf("SERVER: saving %d\n", i);
+
+            latest_prime = i;
+            //nach dem beschreiben von latest_prime wird eine Sekunde gesleept
+            //durch das sleep werden race-conditions unterbunden, da der Client so immer schneller ist als der Server.
+            sleep(1);
+        }
     }    
     return NULL;
 }
@@ -45,14 +57,19 @@ void *serve_number(){
 // int main(int argc, char const *argv[])
 int main()
 {
-    PrimeArray = malloc(ARRAY_SIZE * sizeof(int));
-    pthread_t server; // Thread Array mit Max Num_THREAD Threads
-    // pthread_t threads[NUM_THREADS]; // Thread Array mit Max Num_THREAD Threads
+    // thread identifier für server und client
+    pthread_t server; 
+    pthread_t client; 
     
-    printf("creating server thread\n");
-    pthread_create(&server, NULL, serve_number, NULL);
-    // int server_id = pthread_create(&server, NULL, serve_number, NULL);
+    //create thread server mit funktion "find_primes"
+    pthread_create(&server, NULL, find_primes, NULL);
+
+    //create thread client mit funktion "print_prime"
+    pthread_create(&client, NULL, print_prime, NULL);
     
-    printf("server thread finished\n");
+    // Code ab hier ist effektiv nutzlos, da server und client in dauerschleife sind.
+    pthread_join(client, NULL);
+    pthread_join(server, NULL);
+
     return EXIT_SUCCESS;
 }
